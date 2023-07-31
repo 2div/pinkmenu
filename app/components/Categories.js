5674f2a73a5c47df063ef21282a8d2e5e60b4a42
2div <mhersi82@gmail.com>
2023-07-31 15:44:29 +0300

    updated

--- app/components/Categories.js
+++ app/components/Categories.js
@@ -1,33 +1,33 @@
-"use client"
+// "use client"
 
-import React, { useState } from 'react';
-import Menu from './Menu';
+// import React, { useState } from 'react';
+// import Menu from './Menu';
 
-const Categories = ({ categories, filterItems }) => {
-  let [currentCategory, setCurrentCategory] = useState(0);
+// const Categories = ({ categories, filterItems }) => {
+//   let [currentCategory, setCurrentCategory] = useState(0);
 
-  const handleCategoryClick = (categoryIndex, category) => {
-    filterItems(category);
-    setCurrentCategory(categoryIndex);
+//   const handleCategoryClick = (categoryIndex, category) => {
+//     filterItems(category);
+//     setCurrentCategory(categoryIndex);
     
-  };
+//   };
 
-  return (
-    <div className="btn-container">
-      {categories.map((category, index) => {
-        return (
-          <button
-            type="button"
-            key={index}
-            className={`filter-btn ${index === currentCategory ? 'active' : ''}`}
-            onClick={() => handleCategoryClick(index, category)}
-          >
-            {category}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+//   return (
+//     <div className="btn-container">
+//       {categories.map((category, index) => {
+//         return (
+//           <button
+//             type="button"
+//             key={index}
+//             className={`filter-btn ${index === currentCategory ? 'active' : ''}`}
+//             onClick={() => handleCategoryClick(index, category)}
+//           >
+//             {category}
+//           </button>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
-export default Categories;
+// export default Categories;
